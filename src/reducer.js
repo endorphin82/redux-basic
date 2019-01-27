25df38fb828a3bf1cb50df57faf896aa033a1d11
endorphin82 <endorphin82@gmail.com>
2019-01-27 10:08:16 +0200

    007-actions

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,23 @@
 let state = 0;
 
-const updateState = (state, action) => {
-  if (action === 'INCREMENT') {
-    return state + 1;
-  } else if (action === 'DECREMENT') {
-    return state - 1;
+const updateState = (state, { type, amount }) => {
+  if (type === "INCREMENT") {
+    return state + amount;
+  } else if (type === "DECREMENT") {
+    return state - amount;
   } else {
     return state;
   }
 };
 
-state = updateState(state, 'INCREMENT');
+const incrementAction = { type: "INCREMENT", amount: 5 };
+const decrementAction = { type: "DECREMENT", amount: 3 };
+
+state = updateState(state, incrementAction);
 console.log(state);
 
-state = updateState(state, 'DECREMENT');
+state = updateState(state, decrementAction);
 console.log(state);
 
-state = updateState(state, 'NOTHING');
+state = updateState(state, {});
 console.log(state);
