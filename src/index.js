53f9d1683281af958cd11c87356351388636ab8e
endorphin82 <endorphin82@gmail.com>
2019-01-30 08:54:56 +0200

    021-store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
+import reducer from "./reducers";
 import todos from "./todos";
 import App from "./App";
+import {
+  addTodo,
+  deleteTodo,
+  editTodo,
+  toggleTodo
+} from "./actions";
 
-ReactDOM.render(<App initialData={todos} />, document.getElementById("root"));
+const store = createStore(reducer, todos);
+// console.log(store.getState());
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(addTodo("Проверить хранилище"));
+store.dispatch(toggleTodo(5));
+store.dispatch(editTodo(5, 'Проверили хранилище'))
+store.dispatch(deleteTodo(3));
+
+ReactDOM.render(<App initialData={todos}/>, document.getElementById("root"));
