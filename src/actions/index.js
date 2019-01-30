2535aa8916697418d122f7c4c18b8841ea0b106a
endorphin82 <endorphin82@gmail.com>
2019-01-30 08:35:26 +0200

    020-functions_for_create_actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,36 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const TOGGGLE_TODO = 'TOGGGLE_TODO';
-export const EDIT_TODO = 'EDIT_TODO';
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const TOGGGLE_TODO = "TOGGGLE_TODO";
+export const EDIT_TODO = "EDIT_TODO";
+
+let nextId = 5;
+
+export function addTodo(title) {
+  return {
+    type: ADD_TODO,
+    id: nextId++,
+    title
+  };
+}
+
+export function deleteTodo(id) {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+}
+
+export function toggleTodo(id) {
+  return {
+    type: TOGGGLE_TODO,
+    id
+  };
+}
+
+export function editTodo(id, title) {
+  return {
+    type: EDIT_TODO,
+    id,
+    title
+  };
+}
