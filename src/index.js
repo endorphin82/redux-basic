f3a938bc0db735988d2c6bd5af25ece22dbcd970
endorphin82 <endorphin82@gmail.com>
2019-01-31 07:28:09 +0200

    025-026-create_stateful_with_connect

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import reducer from "./reducers";
-import todos from "./todos";
+import state from "./state";
 import App from "./App";
 
-const store = createStore(reducer, todos);
+const store = createStore(reducer, state);
 
 ReactDOM.render(
   <Provider store={store}>
